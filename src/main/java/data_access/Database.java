511193551ae570040e896c75a1316f59825b100f
humzahahmed <hu.ahmed@mail.utoronto.ca>
2026-08-01 18:11:05 -0400

    database setup

--- src/main/java/data_access/Database.java
+++ src/main/java/data_access/Database.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Database {
 
@@ -16,7 +17,14 @@ public class Database {
 
     public static Connection connect() throws SQLException {
         createDataDirectory();
-        return DriverManager.getConnection(URL);
+
+        final Connection connection = DriverManager.getConnection(URL);
+
+        try (Statement statement = connection.createStatement()) {
+            statement.execute("PRAGMA foreign_keys = ON;");
+        }
+
+        return connection;
     }
 
     private static void createDataDirectory() throws SQLException {
